e7a609535d57fba9bafe8140a99a4b21c1379a34
Mustapha Elkhatib <melkhat1@ford.com>
2018-05-08 10:37:57 -0400

    adding the test list modify

--- my-project/src/components/RestaurantList.vue
+++ my-project/src/components/RestaurantList.vue
@@ -15,22 +15,22 @@
             const generateData2 = _ => {
                 const data = [];
                 const states = ['La Penguina', 'Restaurant 2', 'Restaurant 3', 'Restaurant 4', 'Restaurant 5', 'Restaurant 2', 'Restaurant 2 '];
-                const initials = ['CA', 'IL', 'MD', 'TX', 'FL', 'CO', 'CT'];
+                // const initials = ['CA', 'IL', 'MD', 'TX', 'FL', 'CO', 'CT'];
                 states.forEach((city, index) => {
                     data.push({
                         label: city,
-                        key: index,
-                        initial: initials[index]
+                        key: index
+                        // initial: initials[index]
                     });
                 });
                 return data;
             };
             return {
                 data2: generateData2(),
-                value2: [],
-                filterMethod(query, item) {
-                    return item.initial.toLowerCase().indexOf(query.toLowerCase()) > -1;
-                }
+                value2: []
+                // filterMethod(query, item) {
+                //     return item.initial.toLowerCase().indexOf(query.toLowerCase()) > -1;
+                // }
             };
         }
     };
